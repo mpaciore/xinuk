fdbc95c15cc0265353d4b0ad3e2d3074a95bd283
ddworak <dawid.dworak@gmail.com>
2017-06-08 23:56:07 +0200

    Multiple node runner.

--- scripts/prometheus.sh
+++ scripts/prometheus.sh
@@ -2,7 +2,7 @@
 ## Job name
 #SBATCH -J foraminifera
 ## Number of nodes to allocate
-#SBATCH -N 1
+#SBATCH -N 2
 ## Number of tasks per node (by default number of cores per node to allocate)
 #SBATCH --ntasks-per-node=24
 #SBATCH --mem-per-cpu=4GB
@@ -16,32 +16,45 @@ cd ${SLURM_SUBMIT_DIR}
 module add plgrid/tools/java8/1.8.0_60
 
 SUPERVISOR_HOSTNAME=`/bin/hostname`
+ITERATIONS_NUMBER=1000
+GRID_SIZE=100
+WORKERS_ROOT=9
 
 echo $SUPERVISOR_HOSTNAME
-
-HOSTNAMES=()
-for HOST in `scontrol show hostnames`;
-do
-    if [ "$HOST" != "$SUPERVISOR_HOSTNAME" ]; then
-        HOSTNAMES+=(${HOST})
-    fi
-done
+HOSTSS=`scontrol show hostnames`
+HOSTNAMES=`echo $HOSTSS | sed "s/\b$SUPERVISOR_HOSTNAME\b//g"`
 
 mkdir -p ${SLURM_SUBMIT_DIR}/results/${SLURM_JOB_ID}
-##mkdir -p ${SCRATCH}/results/${SLURM_JOB_ID}
+
+for WORKER_HOST in $HOSTNAMES
+    do
+	echo $WORKER_HOST
+        srun -w${WORKER_HOST} -c${SLURM_NTASKS_PER_NODE} -N1 -n1 \
+            ${JAVA_HOME}/bin/java \
+            -Xmx65536m -Xms512m -XX:+UseG1GC \
+            -Dclustering.ip=${WORKER_HOST} \
+            -Dclustering.supervisor.ip=${SUPERVISOR_HOSTNAME} \
+            -Dlog.name=${SLURM_SUBMIT_DIR}/results/${SLURM_JOB_ID}/${WORKER_HOST} \
+            -Dformin.config.iterationsNumber=${ITERATIONS_NUMBER} \
+            -Dformin.config.gridSize=${GRID_SIZE} \
+            -Dformin.config.workersRoot=${WORKERS_ROOT} \
+            -Dakka.cluster.min-nr-of-members=2 \
+            -jar formin.jar &
+    done
+
+sleep 15
 
 ${JAVA_HOME}/bin/java \
     -Xmx65536m -Xms512m -XX:+UseG1GC \
     -Dclustering.ip=${SUPERVISOR_HOSTNAME} \
     -Dclustering.supervisor.ip=${SUPERVISOR_HOSTNAME} \
+    -Dlog.name=${SLURM_SUBMIT_DIR}/results/${SLURM_JOB_ID}/${SUPERVISOR_HOSTNAME} \
     -Dformin.config.isSupervisor=true \
-    -Dformin.config.iterationsNumber=1000 \
-    -Dformin.config.gridSize=100 \
-    -Dformin.config.workersRoot=6 \
-    -Dakka.cluster.min-nr-of-members=1 \
+    -Dformin.config.iterationsNumber=${ITERATIONS_NUMBER} \
+    -Dformin.config.gridSize=${GRID_SIZE} \
+    -Dformin.config.workersRoot=${WORKERS_ROOT} \
+    -Dakka.cluster.min-nr-of-members=2 \
     -jar formin.jar
-
-mv forminData.txt ${SLURM_SUBMIT_DIR}/results/${SLURM_JOB_ID}/data.txt
 
 sleep 5
 exit 0
