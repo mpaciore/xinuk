871dcb306b698d1d08959ce573293fce7c251a81
Xibuk <kubixb@gmail.com>
2017-11-16 20:18:12 +0100

    Small refactor. Add test for calculating possible destinations in MovesController.

--- src/main/scala/pl/edu/agh/formin/algorithm/MovesController.scala
+++ src/main/scala/pl/edu/agh/formin/algorithm/MovesController.scala
@@ -5,7 +5,6 @@ import com.avsystem.commons.SharedExtensions._
 import com.avsystem.commons.misc.Opt
 import org.slf4j.Logger
 import pl.edu.agh.formin.WorkerActor.MetricsMarker
-import pl.edu.agh.formin.WorkerId
 import pl.edu.agh.formin.config.ForminConfig
 import pl.edu.agh.formin.model._
 
@@ -42,6 +41,38 @@ final class MovesController(bufferZone: TreeSet[(Int, Int)], logger: Logger)(imp
     val metrics = Metrics(foraminiferaCount, algaeCount, 0, config.foraminiferaStartEnergy.value * foraminiferaCount, 0, 0, 0, 0)
     logMetrics(1, metrics)
     grid
+  }
+
+
+  def calculatePossibleDestinations(cell: ForaminiferaCell, x: Int, y: Int, grid : Grid): Iterator[(Int, Int, GridPart)] = {
+    val neighbourCellCoordinates = Grid.neighbourCellCoordinates(x, y)
+
+    val destinations = Grid.SubcellCoordinates
+      .map { case (i, j) => cell.smell(i)(j) }
+      .zipWithIndex
+      .sorted(implicitly[Ordering[(Signal, Int)]].reverse)
+      .iterator
+      .map { case (_, idx) =>
+        val (i, j) = neighbourCellCoordinates(idx)
+        (i, j, grid.cells(i)(j))
+      }
+    destinations
+  }
+
+  def selectDestinationCell(possibleDestinations: Iterator[(Int, Int, GridPart)], newGrid : Grid): commons.Opt[(Int, Int, SmellingCell with ForaminiferaAccessible with Product with Serializable)] = {
+    val destinationCell = possibleDestinations
+      .collectFirstOpt {
+        case (i, j, destination: AlgaeCell) =>
+          (i, j, destination)
+        case (i, j, destination: EmptyCell) =>
+          val effectiveDestination = newGrid.cells(i)(j) match {
+            case newAlgae: AlgaeCell =>
+              newAlgae
+            case _ => destination
+          }
+          (i, j, effectiveDestination)
+      }
+    destinationCell
   }
 
   def makeMoves(iteration: Long, grid: Grid): Grid = {
@@ -117,45 +148,16 @@ final class MovesController(bufferZone: TreeSet[(Int, Int)], logger: Logger)(imp
       foraminiferaReproductionsCount += 1
     }
 
-    def calculatePossibleDestinations(cell: ForaminiferaCell, x: Int, y: Int): Iterator[(Int, Int, GridPart)] = {
-      val neighbourCellCoordinates = Grid.neighbourCellCoordinates(x, y)
-
-      val destinations = Grid.SubcellCoordinates
-        .map { case (i, j) => cell.smell(i)(j) }
-        .zipWithIndex
-        .sorted(implicitly[Ordering[(Signal, Int)]].reverse)
-        .iterator
-        .map { case (_, idx) =>
-          val (i, j) = neighbourCellCoordinates(idx)
-          (i, j, grid.cells(i)(j))
-        }
-      destinations
-    }
-
-    def selectDestinationCell(possibleDestinations: Iterator[(Int, Int, GridPart)]): commons.Opt[(Int, Int, SmellingCell with ForaminiferaAccessible with Product with Serializable)] = {
-      val destinationCell = possibleDestinations
-        .collectFirstOpt {
-          case (i, j, destination: AlgaeCell) =>
-            consumedAlgaeCount += 1
-            algaeTotalLifespan += destination.lifespan
-            (i, j, destination)
-          case (i, j, destination: EmptyCell) =>
-            val effectiveDestination = newGrid.cells(i)(j) match {
-              case newAlgae: AlgaeCell =>
-                consumedAlgaeCount += 1
-                algaeTotalLifespan += newAlgae.lifespan
-                newAlgae
-              case _ => destination
-            }
-            (i, j, effectiveDestination)
-        }
-      destinationCell
-    }
-
     def moveForaminifera(cell: ForaminiferaCell, x: Int, y: Int): Unit = {
-      val destinations = calculatePossibleDestinations(cell, x, y)
-      selectDestinationCell(destinations) match {
+      val destinations = calculatePossibleDestinations(cell, x, y, grid)
+      selectDestinationCell(destinations, newGrid) match {
         case Opt((i, j, destinationCell)) =>
+          destinationCell match {
+            case AlgaeCell(_, lifespan) =>
+              consumedAlgaeCount += 1
+              algaeTotalLifespan += lifespan
+            case _ =>
+          }
           newGrid.cells(i)(j) = destinationCell.withForaminifera(cell.energy - config.foraminiferaLifeActivityCost, cell.lifespan + 1)
           newGrid.cells(x)(y) = EmptyCell(cell.smell)
         case Opt.Empty =>
---
+++ src/test/scala/pl.edu.agh.formin/MovesControllerTest.scala
@@ -0,0 +1,57 @@
+package pl.edu.agh.formin
+import com.avsystem.commons.misc.Opt
+import org.scalatest.{BeforeAndAfter, FlatSpecLike, Matchers}
+import org.slf4j.LoggerFactory
+import pl.edu.agh.formin.algorithm.MovesController
+import pl.edu.agh.formin.config.{ForminConfig, GuiType}
+import pl.edu.agh.formin.model._
+
+import scala.collection.immutable.TreeSet
+
+class MovesControllerTest extends FlatSpecLike with Matchers with BeforeAndAfter {
+  implicit val config: ForminConfig = ForminConfig(
+    foraminiferaStartEnergy = Energy(0.5),
+    foraminiferaReproductionCost = Energy(0.2),
+    foraminiferaReproductionThreshold = Energy(0.3),
+    foraminiferaLifeActivityCost = Energy(0.1),
+    algaeReproductionFrequency = 2,
+    algaeEnergeticCapacity = Energy(0.4),
+    signalSpeedRatio = 2,
+    signalSuppressionFactor = 0.5,
+    gridSize = 5,
+    spawnChance = 0.1,
+    foraminiferaSpawnChance = 0.5,
+    foraminiferaInitialSignal = Signal(-1),
+    algaeInitialSignal = Signal(1),
+    guiType = GuiType.None,
+    guiCellSize = 4,
+    workersRoot = 1,
+    iterationsNumber = 1000,
+    isSupervisor = true
+  )
+
+  private var grid: Grid = _
+
+  before {
+    grid = Grid.empty(Set.empty)
+  }
+
+  "A calculatePossibleDestinations method" should "return correct possible destination cells" in {
+    var movesController = new MovesController(TreeSet.empty, LoggerFactory.getLogger(""))
+    val cell1 = EmptyCell.Instance.withForaminifera(config.foraminiferaStartEnergy, 0)
+    grid.cells(2)(2) = cell1
+    grid.cells(3)(2) = EmptyCell.Instance.withForaminifera(config.foraminiferaStartEnergy, 0)
+    grid.cells(2)(3) = EmptyCell.Instance.withForaminifera(config.foraminiferaStartEnergy, 0)
+
+    grid.cells(2)(2).smell(0)(0) = Signal(20)
+    grid.cells(2)(2).smell(0)(2) = Signal(15)
+    grid.cells(2)(2).smell(1)(0) = Signal(-5)
+    grid.cells(2)(2).smell(2)(2) = Signal(-666)
+
+    val (x, y, destination) = movesController.calculatePossibleDestinations(cell1, 2, 2, grid).next()
+
+    x shouldBe 1
+    y shouldBe 1
+    destination shouldBe grid.cells(1)(1)
+  }
+}
