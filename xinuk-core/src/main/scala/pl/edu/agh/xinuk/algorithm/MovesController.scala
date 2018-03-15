c1d01b22da36664138bf5fdc78171c26ad591be9
ddworak <dawid.dworak@gmail.com>
2018-03-15 22:40:55 +0100

    Generic possible destinations calculation

--- xinuk-core/src/main/scala/pl/edu/agh/xinuk/algorithm/MovesController.scala
+++ xinuk-core/src/main/scala/pl/edu/agh/xinuk/algorithm/MovesController.scala
@@ -1,10 +1,23 @@
 package pl.edu.agh.xinuk.algorithm
 
-import pl.edu.agh.xinuk.model.Grid
+import pl.edu.agh.xinuk.model.{Grid, GridPart, Signal}
 import pl.edu.agh.xinuk.simulation.Metrics
 
 trait MovesController {
   def initialGrid: (Grid, Metrics)
 
   def makeMoves(iteration: Long, grid: Grid): (Grid, Metrics)
+
+  protected final def calculatePossibleDestinations(x: Int, y: Int, grid: Grid): Iterator[(Int, Int, GridPart)] = {
+    val neighbourCellCoordinates = Grid.neighbourCellCoordinates(x, y)
+    Grid.SubcellCoordinates
+      .map { case (i, j) => grid.cells(x)(y).smell(i)(j) }
+      .zipWithIndex
+      .sorted(implicitly[Ordering[(Signal, Int)]].reverse)
+      .iterator
+      .map { case (_, idx) =>
+        val (i, j) = neighbourCellCoordinates(idx)
+        (i, j, grid.cells(i)(j))
+      }
+  }
 }
