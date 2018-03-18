f6a39a07f4ccd508c401bf356977430667a62862
ddworak <dawid.dworak@gmail.com>
2018-03-18 17:57:32 +0100

    Fortwist main runner

---
+++ fortwist/src/main/scala/pl/edu/agh/fortwist/Main.scala
@@ -0,0 +1,26 @@
+package pl.edu.agh.fortwist
+
+import com.typesafe.scalalogging.LazyLogging
+import pl.edu.agh.fortwist.algorithm.FortwistMovesController
+import pl.edu.agh.fortwist.model.parallel.FortwistConflictResolver
+import pl.edu.agh.xinuk.Simulation
+
+object Main extends LazyLogging {
+  private val configPrefix = "fortwist"
+  private val metricHeaders = Vector(
+    "foraminiferaCount",
+    "algaeCount",
+    "foraminiferaDeaths",
+    "foraminiferaTotalEnergy",
+    "foraminiferaReproductionsCount",
+    "consumedAlgaeCount",
+    "foraminiferaTotalLifespan",
+  )
+
+  def main(args: Array[String]): Unit = {
+    import pl.edu.agh.xinuk.config.ValueReaders._
+    new Simulation(configPrefix, metricHeaders, FortwistConflictResolver)(new FortwistMovesController(_)(_)).start()
+  }
+
+}
+
