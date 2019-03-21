8dba325b254e92772c7ca0553ce1c45b09ff5d7d
Mateusz Paciorek <mateusz.j.paciorek@gmail.com>
2019-03-21 14:24:23 +0100

    Add mock simulation for signal propagation visualization

---
+++ mock/src/main/scala/pl.edu.agh.mock/simulation/MockMetrics.scala
@@ -0,0 +1,20 @@
+package pl.edu.agh.mock.simulation
+
+import pl.edu.agh.xinuk.simulation.Metrics
+
+final case class MockMetrics() extends Metrics {
+  override def log: String = {
+    s""
+  }
+
+  override def series: Vector[(String, Double)] = Vector()
+
+  override def +(other: Metrics): MockMetrics = {
+    this
+  }
+}
+
+object MockMetrics {
+  private val EMPTY = MockMetrics()
+  def empty(): MockMetrics = EMPTY
+}
