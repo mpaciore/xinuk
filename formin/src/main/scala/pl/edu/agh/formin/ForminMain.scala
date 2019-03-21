d9030585e9217e435e710a829600ecf4aff971a3
Mateusz Paciorek <mateusz.j.paciorek@gmail.com>
2019-03-21 14:19:01 +0100

    Layout windows in grid, predefine colors for simulations

--- formin/src/main/scala/pl/edu/agh/formin/ForminMain.scala
+++ formin/src/main/scala/pl/edu/agh/formin/ForminMain.scala
@@ -1,10 +1,14 @@
 package pl.edu.agh.formin
 
+import java.awt.Color
+
 import com.typesafe.scalalogging.LazyLogging
 import pl.edu.agh.formin.algorithm.ForminMovesController
 import pl.edu.agh.formin.config.ForminConfig
 import pl.edu.agh.formin.model.parallel.ForminConflictResolver
+import pl.edu.agh.formin.model.{AlgaeCell, ForaminiferaCell}
 import pl.edu.agh.xinuk.Simulation
+import pl.edu.agh.xinuk.model.SmellingCell
 
 object ForminMain extends LazyLogging {
   private val configPrefix = "formin"
@@ -19,9 +23,19 @@ object ForminMain extends LazyLogging {
     "algaeTotalLifespan"
   )
 
+  private def cellToColor(cell: SmellingCell): Color = {
+    cell match {
+      case AlgaeCell(_, _) => new Color(0, 128, 0)
+      case ForaminiferaCell(_, _, _) => new Color(139, 69, 19)
+      case _ => Color.WHITE
+    }
+  }
+
   def main(args: Array[String]): Unit = {
     import pl.edu.agh.xinuk.config.ValueReaders._
-    new Simulation[ForminConfig](configPrefix, metricHeaders, ForminConflictResolver)(new ForminMovesController(_)(_)).start()
+    new Simulation[ForminConfig](configPrefix, metricHeaders, ForminConflictResolver)(new ForminMovesController(_)(_),
+      { case cell: SmellingCell => cellToColor(cell) }
+    ).start()
   }
 
 }
--- torch/src/main/scala/pl.edu.agh.torch/TorchMain.scala
+++ torch/src/main/scala/pl.edu.agh.torch/TorchMain.scala
@@ -1,9 +1,13 @@
 package pl.edu.agh.torch
 
+import java.awt.Color
+
 import com.typesafe.scalalogging.LazyLogging
 import pl.edu.agh.torch.algorithm.TorchMovesController
 import pl.edu.agh.torch.model.parallel.TorchConflictResolver
+import pl.edu.agh.torch.model.{EscapeCell, FireCell, HumanCell}
 import pl.edu.agh.xinuk.Simulation
+import pl.edu.agh.xinuk.model.SmellingCell
 
 object TorchMain extends LazyLogging {
   private val configPrefix = "torch"
@@ -15,9 +19,20 @@ object TorchMain extends LazyLogging {
     "peopleEscapes"
   )
 
+  private def cellToColor(cell: SmellingCell): Color = {
+    cell match {
+      case HumanCell(_, _, _) => Color.BLUE
+      case FireCell(_) => Color.ORANGE
+      case EscapeCell(_) => new Color(139, 69, 19)
+      case _ => Color.WHITE
+    }
+  }
+
   def main(args: Array[String]): Unit = {
     import pl.edu.agh.xinuk.config.ValueReaders._
-    new Simulation(configPrefix, metricHeaders, TorchConflictResolver)(new TorchMovesController(_)(_)).start()
+    new Simulation(configPrefix, metricHeaders, TorchConflictResolver)(new TorchMovesController(_)(_),
+      { case cell: SmellingCell => cellToColor(cell) }
+    ).start()
   }
 
 }
