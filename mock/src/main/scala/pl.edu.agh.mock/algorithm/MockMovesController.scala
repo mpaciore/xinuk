f80331fc10e7318743275a7cefbe2ed842b101ad
pecatoma <pecatoma@gmail.com>
2019-05-29 21:11:36 +0200

    Change the way that signal is interpreted

--- mock/src/main/scala/pl.edu.agh.mock/algorithm/MockMovesController.scala
+++ mock/src/main/scala/pl.edu.agh.mock/algorithm/MockMovesController.scala
@@ -33,7 +33,7 @@ final class MockMovesController(bufferZone: TreeSet[(Int, Int)])(implicit config
   override def makeMoves(iteration: Long, grid: Grid): (Grid, MockMetrics) = {
 
     val newGrid = Grid.empty(bufferZone,workerId = grid.workerId)
-    //Thread.sleep(100)
+  //  Thread.sleep(100)
 
     def copyCells(x: Int, y: Int, cell: GridPart): Unit = {
       newGrid.cells(x)(y) = cell
