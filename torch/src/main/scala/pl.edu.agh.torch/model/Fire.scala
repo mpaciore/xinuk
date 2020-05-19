12ed64d1ff214990da0b7e67289caf44f87cd8b1
Mateusz Paciorek <mateusz.j.paciorek@gmail.com>
2020-05-19 15:13:13 +0200

    Adapt Torch to new grid format

--- torch/src/main/scala/pl.edu.agh.torch/model/Fire.scala
+++ torch/src/main/scala/pl.edu.agh.torch/model/Fire.scala
@@ -2,7 +2,7 @@ package pl.edu.agh.torch.model
 
 import pl.edu.agh.torch.config.TorchConfig
 import pl.edu.agh.xinuk.model.Cell.SmellMap
-import pl.edu.agh.xinuk.model.{BufferCell, EmptyCell, GridPart, SmellingCell}
+import pl.edu.agh.xinuk.model.{EmptyCell, GridPart, SmellingCell}
 
 
 final case class FireCell(smell: SmellMap) extends SmellingCell {
@@ -14,7 +14,15 @@ final case class FireCell(smell: SmellMap) extends SmellingCell {
 trait FireAccessible[+T <: GridPart] {
   def withFire(): T
 }
+
 object FireAccessible {
+
+  def unapply(arg: GridPart)(implicit config: TorchConfig): Option[FireAccessible[GridPart]] = arg match {
+    case cell: EmptyCell => Some(unapply(cell))
+    case cell: EscapeCell => Some(unapply(cell))
+    case cell: HumanCell => Some(unapply(cell))
+    case _ => None
+  }
 
   def unapply(arg: EmptyCell)(implicit config: TorchConfig): FireAccessible[FireCell] =
     new FireAccessible[FireCell] {
@@ -30,17 +38,4 @@ object FireAccessible {
     new FireAccessible[FireCell] {
       override def withFire(): FireCell = FireCell(arg.smellWith(config.fireInitialSignal))
     }
-
-  def unapply(arg: BufferCell)(implicit config: TorchConfig): FireAccessible[BufferCell] =
-    new FireAccessible[BufferCell] {
-      override def withFire(): BufferCell = BufferCell(FireCell(arg.smellWith(config.fireInitialSignal)))
-    }
-
-  def unapply(arg: GridPart)(implicit config: TorchConfig): Option[FireAccessible[GridPart]] = arg match {
-    case cell: EmptyCell => Some(unapply(cell))
-    case cell: EscapeCell => Some(unapply(cell))
-    case cell: BufferCell => Some(unapply(cell))
-    case cell: HumanCell => Some(unapply(cell))
-    case _ => None
-  }
 }
